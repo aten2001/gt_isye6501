25622401d4445309a278b1ec5bbe81b425665b2d
lwileczek <30635219+lwileczek@users.noreply.github.com>
2018-09-10 21:17:49 -0400

    Update q62.R

--- hw3/q62.R
+++ hw3/q62.R
@@ -13,7 +13,7 @@ climate_data <- read.table(
 )
 
 # CUSUM function
-cusum <- function(my_vector, threshold, C = 0, type="high"){
+cusum <- function(my_vector, threshold = 100, C = 0, type="high"){
   # Type - high/low, what we are looking for. 
   # higher change or lower change. 
   # threshold, is our threshold
@@ -24,53 +24,60 @@ cusum <- function(my_vector, threshold, C = 0, type="high"){
     print("type is not low or high")
     stop()
   }
+  output <- 0
   n = length(my_vector)
-  cs <- rep(0, (n-1))
-  if (type == "high"){
-    # CUSUM formula: st = max{0, s(t-1) + (xt - mu - C)}
-    deltas <- my_vector - mean(my_vector) - C
-  } else {
-    # CUSUM formula: st = max{0, s(t-1) + (mu - xt - C)}
-    deltas <- mean(my_vector) - my_vector  - C
+  # for each time step
+  for (t in 2:n){
+    # use rolling avg. because if it was the real world we wouldn't know 
+    # future points to use them in our average.
+    mu <- mean(my_vector[1:t])  # rolling avg. 
+    if (type == "high"){
+      # CUSUM formula: st = max{0, s(t-1) + (xt - mu - C)}
+      deltas <- my_vector[1:t] - mu - C
+    } else {
+      # CUSUM formula: st = max{0, s(t-1) + (mu - xt - C)}
+      deltas <- mu - my_vector[1:t]  - C
+    }
+    
+    st = max(0, deltas[1])
+    for (i in 2:t){
+      st <- max(0, st + deltas[i] )
+    }
+    
+    # check if pasted the threshold
+    if (st >= threshold){
+      print(paste("CUSUM over threshold @", t))
+      output <- t
+      break
+    }
   }
-  
-  cs[1] = max(0, deltas[1])
-  for (i in 2:(n-1)){
-    cs[i] <- max(0, cs[i-1] + deltas[i] )
-  }
-  
-  # check if pasted the threshold
-  bln <- cs > threshold
-  
-  return(list(
-    indices=which(bln), 
-    cs_over_vals=cs[bln], 
-    over_vals=my_vector[bln], 
-    cum_vals=cs
-    )
-  )
+  return(output) # 0 if never go over threshold. 
 }
 
 
 # now to figure out a proper C & threshold. 80 looks about good. 
 # test <- cusum(climate_data[, 3], threshold = 86, C = 5, type="low")
 
+indices <- rep(0, 20)  # initiate vector
 for (year in 2:21){
   output <- cusum(
     climate_data[, year], 
-    threshold = 10, 
-    C = 2, 
+    threshold = 15, 
+    C = 5, 
     type="low"
-    )
+  )
   
-  first_date <- climate_data[output$indices[1], 1]
+  indices[year - 1] <- output
+  first_date <- climate_data[output, 1]
   print(first_date)
 }
+print(paste("median date:", climate_data[median(indices), 1]))
 
 library(ggplot2)
 climate_data$DAY <- factor(climate_data$DAY, levels = climate_data$DAY)
 p <- ggplot(climate_data, aes(x = DAY, y = X2007) ) + geom_point(stat = "identity")
-p + scale_x_discrete(breaks = climate_data[seq(1, 123, by=10), 1])
+p + scale_x_discrete(breaks = climate_data[seq(1, 123, by=10), 1]) 
+
 
 # part 2
 rm(list = ls())
@@ -81,24 +88,23 @@ climate_data <- read.table(
 )
 
 # until September 21 end of summer. 
-summer <- climate_data[1:83, 2:21] # only summer months
-avg_summer_temp <- colMeans(summer)  # avg. temp in each summer
+summer <- climate_data[1:83, 2:21]
+avg_summer_temp <- colMeans(summer)
 cusum2 <- function(data, threshold=5, C=0){
-  # don't want to take avg of tmps in the future not real world scenario
-  for (k in 2:length(data)){ 
-    mu = mean(data[1:k])  # avg. until now
+  for (k in 2:length(data)){
+    mu = mean(data[1:k])
     deltas <- data - mu - C
     st <- deltas[1]
     for (i in 1:k){
        st <- max(0, st + deltas[i])
-       if (st > threshold) break  # if we went over threshold stop loop
+       if (st > threshold) break
     }
-    if (st > threshold) { # have to check again because we only broke out of inner loop
-      output <- k # save the year that put us over
-      print(paste("Over threshold @", k))  # it's just the index
+    if (st > threshold) {
+      output <- k
+      print(paste("Over threshold @", k))
       break
     } else {
-      output <- 0  # never went over
+      output <- 0
     }
   }
   return(output)
@@ -107,5 +113,5 @@ cusum2 <- function(data, threshold=5, C=0){
 year <- cusum2(avg_summer_temp, threshold = 5, C = 1) 
 
 if (year > 0){
-  print(paste("Year it's definitely hotter:", colnames(summer)[year]))  # turn index into year.
+  print(paste("Year it's definitely hotter:", colnames(summer)[year]))
 }
