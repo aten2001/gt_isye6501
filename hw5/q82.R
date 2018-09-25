737c2b350dc49de041d6e87ec4ede7608f1195bf
lwileczek <wileczek105@gmail.com>
2018-09-24 22:25:10 -0400

    most of code

--- hw5/q82.R
+++ hw5/q82.R
@@ -1,6 +1,9 @@
+#!/usr/bin/Rscript
 # Homework 5
+# vim: tw=80 ts=2 sw=2:
 
 library(boot)
+library(ggplot2)
 
 crimes <- read.table("uscrime.txt", header=T)
 
@@ -18,4 +21,46 @@ cv.glm(crimes, my_fit)$delta  # 75308.54
 ## Feels weird with such a short homework. Our error is ~MSE.
 ## Some people like to look at RMSE because it puts the error 
 ## on the same order as the response (generally). for us, that 
-## Would be, 275.424
+## Would be, 275.424. This does not imply our average error is 
+## 275. A common miss-interpretation. 
+
+## Let's take a quick look at the features to see if we can 
+## Remove some. 
+print(summary(my_fit))
+
+## After seeing the results it appears as though a few of 
+## the features are unnecessary. I'm going to perform a 
+## simple backward model selection, because I want the bias
+## of keeping more varibles in my model, to remove a few 
+## Variables. This will not give us the best model in the 
+## world but it's easy and doesn't go too far past this 
+## Section of the notes
+
+slim_lm <- step(my_fit, direction = "backward")
+
+print(slim_lm$anova)  # Check out who was kept!
+
+cv.glm(crimes, slim_lm)$delta  # 48661.19 
+
+## yay our cv error went down! We can assume our new 
+## linear model fits the data better now. 
+
+sample <- c(
+                 14.0,
+                 0,
+                 10.0,
+                 12.0,
+                 15.5,
+                 0.640,
+                 94.0,
+                 150,
+                 1.1,
+                 0.120,
+                 3.6,
+                 3200,
+                 20.1,
+                 0.04,
+                 39.0
+               )
+
+print(predict(my_fit, newdata=sample))
