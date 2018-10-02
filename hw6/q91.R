0a7eead1d19ddd5543707817b10dc52e46887404
lwileczek <wileczek105@gmail.com>
2018-10-01 20:41:06 -0400

    updating homework

--- hw6/q91.R
+++ hw6/q91.R
@@ -8,16 +8,17 @@ crimes_df <- read.table("uscrime.txt", header=T)
 
 # compute PCA (SVD) 
 crime_pca <- prcomp(
-  crimes_df[, -1], 
+  crimes_df[, -ncol(crimes_df)], 
   center = T, 
   scale = T
 )
 
 # new values after the SVD
-print(crime_pca$x)
+# print(crime_pca$x)
 
 # crime_pca$rotation  # matrix of variable loadings.
-# crime_pca$scale     # scaling used. 
+print("scale")
+print( crime_pca$scale )     # scaling used. 
 
 
 # To put the model back in terms of the original features we use 
@@ -25,7 +26,42 @@ print(crime_pca$x)
 # rotation element of our prcomp() funcion.  
 
 pca_df <- cbind(crime_pca$x, crimes_df[, ncol(crimes_df)])  # add response
+colnames(pca_df)[ncol(pca_df)] <- "response"
 print(head(pca_df))  ## name the last column!
-pca_glm <- glm(pca_df[, ncol(pca_df)]~., data = as.data.frame(pca_df)) 
+pca_glm <- glm(response ~ ., data = as.data.frame(pca_df)) 
 
-print(pca_glm)
+# check the coefficients
+print(pca_glm$coefficients)
+
+sample <- c(
+  14.0,
+  0,
+  10.0,
+  12.0,
+  15.5,
+  0.640,
+  94.0,
+  150,
+  1.1,
+  0.120,
+  3.6,
+  3200,
+  20.1,
+  0.04,
+  39.0
+)
+
+# we need to multiply the coefficients by the rotation and then the scale
+# factors to get the linear model in terms of the original features and unscaled
+
+factors <- crime_pca$rotation
+for (i in 1:15) {
+  factors[, i] * pca_glm$coefficients[i+1]  
+}
+
+# scale sample instead of coefficients 
+sample <- (sample - crime_pca$center)/ crime_pca$scale
+
+ans <- pca_glm$coefficients[1] + sum( factors%*%sample )  # about 900
+
+cat("\n", "Answer:", ans, "\n")
