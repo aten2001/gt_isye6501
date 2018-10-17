c7e6a9891a900eca51b0c2cd5249830554af2e6a
lwileczek <wileczek105@gmail.com>
2018-10-16 22:18:36 -0400

    finishing min hw requirements for code

--- hw8/q111.R
+++ hw8/q111.R
@@ -26,7 +26,7 @@ fit <- glm(
 )
 # AIC: 650
 
-print(fit)
+print(fit) 
 cat(
   "\n",
   "Dev ratio:",
@@ -80,7 +80,10 @@ png('lasso_results.png')
 plot(cv_lasso_fit)
 dev.off()
 
+print("Lasso Coefficients:")
+print("Min Error")
 coef(cv_lasso_fit, s = "lambda.min")
+print("Lambda with error 1 SE above")
 coef(cv_lasso_fit, s = "lambda.1se")
 
 lasso_fit <- glmnet(
@@ -95,8 +98,8 @@ png('lasso_coef_results.png')
 plot(lasso_fit, xvar="lambda", label = TRUE)
 dev.off()
 
-cat("\n", "\n")
-print(cv_lasso_fit)
+# cat("\n", "\n")
+# print(cv_lasso_fit)
 
 opt_lasso_fit <- glmnet(
   x = X,
@@ -108,10 +111,49 @@ opt_lasso_fit <- glmnet(
 
 cat(
   "\n",
-  "Dev ratio:",
+  "Lasso Dev ratio:",
   "\n",
   opt_lasso_fit$dev.ratio,
   "\n",
   sep=""
 )
+
+
+# Now the elastic net
+# I tried a few different alpha values but with the randomness I couldn't find a
+# clear winner. So I'm going to shoot the middle and use alpha = 0.5
+cv_fit <- cv.glmnet(
+  x = X,
+  y = y,
+  family = "gaussian",
+  alpha = 0.5,  # Lasso
+  nlambda = 750,  # 50% more checks than normal
+  nfolds = 5,
+  type.measure = "mse"
+)
+
+net_fit <- glmnet(
+  x = X,
+  y = y,
+  family = "gaussian",
+  alpha = 0.5,  # Lasso
+  lambda = cv_fit$lambda.1se  # 1se instead of min to get a more sparse result
+)
+
+print("Elastic Net coefficients:")
+coef(cv_fit, s = "lambda.1se")
+
+# closer to 0 is better
+cat(
+  "\n",
+  "Elastic Net Dev ratio:",
+  "\n",
+  net_fit$dev.ratio,
+  "\n",
+  sep=""
+)
+
+png('net_results.png')
+plot(cv_fit)
+dev.off()
 
