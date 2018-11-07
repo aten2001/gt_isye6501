087123216b509848aab4e1e0499d360b15115748
lwileczek <wileczek105@gmail.com>
2018-11-06 20:15:36 -0500

    finish 2.2 & 2.3

--- hw11/integer_sim.py
+++ hw11/integer_sim.py
@@ -15,34 +15,25 @@ import pandas as pd
 
 df = pd.read_excel('../data/diet.xls', sheet_name=0)
 requirments = pd.read_excel('../data/diet.xls', sheet_name=1)
-#          Col1 Col2 Col3 Col4 Col5 Col6 Col7  Col8 Col9 Col10 Col11
-# Minimum  1500   30   20  800  130  125   60  1000  400   700    10
-# Maximum  2500  240   70 2000  450  250  100 10000 5000  1500    40
 
-print(df.head())
-print(requirments)
 prob = pulp.LpProblem("The Army Diet Problem", pulp.LpMinimize)
-
-# Variables: LpVariable("name", lower_bound, upper_bound, discrete | continuous)
-# options LpContinuous or LpInteger
-
-##############################    PART 1    ################################### 
-## note to self, part 1 can be rewritten to just to the pandas dataframe. No
-## need to dictionaries when you can index with .loc[]
+food_items = list(df.loc[:, 'Foods'])
 
 # get list of properties each food item has
 prop_names =  list(df.iloc[:, 3:].columns)
 # create a dictionary of varibles, basically each food item
-food_vars = pulp.LpVariable.dicts("Food", df.loc[:, 'Foods'], 0)
+
+# How much of the food
+food_vars = pulp.LpVariable.dicts("Food", food_items, lowBound=0)
 # create a dict of how much each costs
 food_price = {x:y for x,y in df.loc[:, ['Foods', 'Price/ Serving']].values}
 # create a list of dictionaries each that holds the property value
-food_properties = [{food_name:val for food_name,val in zip(df.loc[:, 'Foods'],
+food_properties = [{food_name:val for food_name,val in zip(food_items,
     df.loc[:, col])} for col in prop_names]
 
 # Objective function
 prob += pulp.lpSum([food_price[food]*food_vars[food] 
-    for food in df.loc[:, 'Foods']]), 'Total cost of diet'
+    for food in food_items]), 'Total cost of diet'
 
 for i,prop_dict in enumerate(food_properties):
 # add min value for property as a constraint
@@ -53,6 +44,40 @@ for i,prop_dict in enumerate(food_properties):
     prob += pulp.lpSum([prop_dict[food]*food_vars[food] 
         for food in df.loc[:, 'Foods']]) <= requirments.loc[1,
             prop_names[i]], 'Max'+prop_names[i]
+
+##############################    PART 2    ################################### 
+## Introducing integer programming. Binaries to indicate inclusion
+
+# Part 2.1 if a food is selected it has at least 1/10 serving. 
+food_selected = pulp.LpVariable.dicts("isSelected", food_items, lowBound=0,
+    upBound=1, cat="Integer")
+prob += pulp.lpSum([food_selected[i] for i in food_items]), ('If selected make'
+    'sure at least 1/10 serving')
+
+# part 2.2 At most either broccoli or celery can be chosen 
+prob += pulp.lpSum([food_selected['Frozen Broccoli'], 
+    food_selected['Celery, Raw'] ]) <=1, 'Have Celery or broccoli'
+
+# part 2.3 at least three kinds of meats need to be included
+meats = [
+    'Bologna,Turkey', 
+    'Chicknoodl Soup', 
+    'Frankfurter, Beef', 
+    'Ham,Sliced,Extralean', 
+    'Hamburger W/Toppings', 
+    'Hotdog, Plain', 
+    'Kielbasa,Prk', 
+    'Poached Eggs', 
+    'Pork', 
+    'Roasted Chicken', 
+    'Scrambled Eggs', 
+    'Tofu', 
+    'Vegetbeef Soup', 
+    'White Tuna in Water'
+    ] 
+
+prob += pulp.lpSum([food_selected[meat] for meat in meats])>=3,('At least three'
+    'meats')
 
 # Write the output
 prob.writeLP("smallDiet.lp")
@@ -66,3 +91,4 @@ for v in prob.variables():
         print(v.name, "=", v.varValue)
 # The optimised objective function value is printed to the screen
 print("Total Cost of Ingredients per can = ", pulp.value(prob.objective))
+
